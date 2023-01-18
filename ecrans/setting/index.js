9ac5a32bf3f957dd7e38e5454e790969ae1ccf1a
Romain kabasi <kabasiromain@gmail.com>
2023-01-18 17:06:30 +0100

    feat : adding all settings

--- ecrans/setting/index.js
+++ ecrans/setting/index.js
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, FlatList } from "react-native";
+import React from "react";
+import styles from "./style";
+import { fakeSettings } from "../../fakeData/fakeSetting";
+import SettingItem from "../../composants/settingItem";
 
 const Setting = () => {
   return (
-    <View>
-      <Text>Ecran de parametrage</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require("../../assets/profil.jpg")}
+          style={styles.image}
+        />
 
-export default Setting
+        <View style={styles.userInfo}>
+          <Text style={styles.userName}>Romain Kabashi</Text>
+          <Text style={styles.userEmail}>romainkabashi@gmail.com</Text>
+          <Text>+243 819 600 518</Text>
+        </View>
+      </View>
+
+      <FlatList data={fakeSettings} keyExtractor={item=>item.id} showsVerticalScrollIndicator={false} renderItem={({item}) =>{
+        return <SettingItem item={item}/>
+      }}/>
+
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Setting;
